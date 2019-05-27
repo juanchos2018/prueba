db5af23b50579817d6638205cdea4ab4475385fb
usuario <usuario@LAPTOP-123>
2019-05-27 01:00:54 -0500

    ágrege un if en ellogin

--- slnPrubasREspositorio/PrjEjemplo/Form1.cs
+++ slnPrubasREspositorio/PrjEjemplo/Form1.cs
@@ -16,5 +16,13 @@ namespace PrjEjemplo
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(""=="")
+            {
+
+            }
+        }
     }
 }
